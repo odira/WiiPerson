f39a62c33d81afd8c246437311cad610bee6ff1e
Vladimir Ilin <wiipuri_dev@icloud.com>
2022-10-15 22:31:45 +0300

    Some changes

--- WiiPerson/WiiPersonApp.swift
+++ WiiPerson/WiiPersonApp.swift
@@ -4,12 +4,14 @@ import WiiKit
 @main
 struct WiiPersonApp: App {
     @StateObject var personnelModel = PersonnelModel()
+    @StateObject var positionModel = PositionModel()
     @StateObject var personFilters = PersonFilters()
     
     var body: some Scene {
         WindowGroup {
             PersonsListView()
                 .environmentObject(personnelModel)
+                .environmentObject(positionModel)
                 .environmentObject(personFilters)
         }
     }
